8fd52101103347dd2e47040546b4bcef8183ccbd
Milman <milindmjoshi@live.com>
2024-01-17 21:17:23 -0500

    Add comments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,38 +1,4 @@
-//test questions
-var testQuestion1 = {
-    question: "What does JS stand for?",
-    answers: ["Junior School","JavaScript","Java School","It doesn't stand for anything"],
-    answerIndex: 1
-}
-var testQuestion2 = {
-    question: "How do you declare a variable in JS",
-    answers: ["var","variable","let","Both var and let"],
-    answerIndex: 3
-}
-var testQuestion3 = {
-    question: "Which method can be used to merge an Array to another Array?",
-    answers: ["join","map","concat","flat"],
-    answerIndex: 2
-}
-
-var score = 0;
-
-var highscores = [];
-
-// start current question at index 0
-var currentQuestion = 0;
-
-var test = [testQuestion1, testQuestion2, testQuestion3];
-
-
-
-//get timer div
-var mytimer = document.querySelector("#countdown-timer");
-var timerValue = 10;
-console.log(mytimer);
-mytimer.innerHTML = timerValue;
-mytimer.style.display = "none";
-
+// User query selectors to get doc elements
 var startScreen = document.querySelector(".startscreen");
 var questionScreen = document.querySelector(".questionscreen");
 var question = document.querySelector("#question");
@@ -52,19 +18,57 @@ var userinitial = document.querySelector("#userinitial");
 var highscoresScreen = document.querySelector("#highscoresScreen");
 var scores = document.querySelector("#scores");
 var highScoresLink = document.querySelector(".highscores");
-console.log("final score element: "+ finalscore);
+var mytimer = document.querySelector("#countdown-timer");
 
+
+//test questions
+var testQuestion1 = {
+    question: "What does JS stand for?",
+    answers: ["Junior School","JavaScript","Java School","It doesn't stand for anything"],
+    answerIndex: 1
+}
+var testQuestion2 = {
+    question: "How do you declare a variable in JS",
+    answers: ["var","variable","let","Both var and let"],
+    answerIndex: 3
+}
+var testQuestion3 = {
+    question: "Which method can be used to merge an Array to another Array?",
+    answers: ["join","map","concat","flat"],
+    answerIndex: 2
+}
+// test array is composed of test questions
+var test = [testQuestion1, testQuestion2, testQuestion3];
+
+// variable for keeping score
+var score = 0;
+
+// array for tracking high scores
+var highscores = [];
+
+// index for current test question
+var currentQuestion = 0;
+
+// timer for test in seconds
+var timerValue = 10;
+
+// variable for start timer function
+var startTimer;
+
+// Clears correct or incorrect message
 function clearMessage(){
     console.log("Clear Message");
     correct.style.display = "none";
 }
 
+// shows correct or incorrect message
 function showMessage(message){
     console.log("show message: " + message);
     correct.innerHTML = message;
     correct.style.display = "block";
 }
 
+// shows final score scre
 function showFinalScore(){
     testcomplete.style.display = "block";
     questionScreen.style.display = "none";
@@ -73,6 +77,7 @@ function showFinalScore(){
 
 }
 
+// Show high scores screen
 function showHighScores(){
     questionScreen.style.display = "none";
     startScreen.style.display = "none";
@@ -88,6 +93,7 @@ function showHighScores(){
     highscoresScreen.style.display = "block";
 }
 
+// show start screen
 function showStartScreen(){
     questionScreen.style.display = "none";
     startScreen.style.display = "block";
@@ -98,10 +104,12 @@ function showStartScreen(){
     score = 0;
 }
 
+// Show high scores screen when link is clicked for high scores
 highScoresLink.addEventListener("click",function(){
     showHighScores();
 });
 
+// start test when start button is clicked
 startbutton.addEventListener("click",function(){
     console.log("start button clicked");
     startScreen.style.display = "none";
@@ -113,19 +121,26 @@ startbutton.addEventListener("click",function(){
     startTestTimer();
 });
 
+// Save initials and score into highscores array when submit button is clicked
 submitbutton.addEventListener("click",function(){
     console.log("submit button clicked");
     console.log(userinitial.value)
-    highscores.push(userinitial.value + " - " + score);
+    // Don't do anything if nothing entered
+    if (userinitial.value == "" || userinitial.value.trim() == ""){
+        return;
+    }
+    highscores.push(userinitial.value.toUpperCase() + " - " + score);
     userinitial.value = "";
     showHighScores();
 });
 
+// Go back to start screen when back button is clicked
 backbutton.addEventListener("click",function(){
     console.log("back button clicked");
     showStartScreen();
 });
 
+// Clear high scores array when clear button is clicked
 clearbutton.addEventListener("click",function(){
     console.log("clear button clicked");
     highscores = [];
@@ -135,7 +150,7 @@ clearbutton.addEventListener("click",function(){
 
  
 
-/* Called when question answer is selected */
+/* call back functino when question answer is selected */
 var answered = function (event){
     // alert("question answered");
     event.stopPropagation();
@@ -162,11 +177,13 @@ var answered = function (event){
     showQuestion(++currentQuestion);
 
 }
+// Add callback function for all answer clicks
 answer1.addEventListener("click",answered);
 answer2.addEventListener("click",answered);
 answer3.addEventListener("click",answered);
 answer4.addEventListener("click",answered);
 
+// show question with specified index, if last question then show final score
 function showQuestion(index){
     console.log("show question index " + index);
     if (index < test.length){
@@ -178,6 +195,7 @@ function showQuestion(index){
     }
     else{
         // At end of test. Show final score
+        stopTimer();
         showFinalScore();
 
     }
@@ -185,8 +203,8 @@ function showQuestion(index){
 
 }
 
-var startTimer;
 
+// function to stop timer and hide
 function stopTimer(){
     console.log("stop timer: " + startTimer);
     if (startTimer != undefined){
@@ -198,6 +216,8 @@ function stopTimer(){
     }
 }
 
+// function to start test timer, when timer value is 0 the timer is stopped and final 
+// score is shown
 function startTestTimer(){
      startTimer = setInterval(function(){
         if (timerValue >0){
